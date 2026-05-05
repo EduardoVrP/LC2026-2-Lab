9fddb11d1e1d45bd7c709dafe7c6e26b512a23bf
EduardoVrP <eduardovar1090@ciencias.unam.mx>
2026-05-05 10:55:29 -0600

    Errores de dedo

--- clase6.v
+++ clase6.v
@@ -1,118 +1,104 @@
 Section Ejemplos.
-    Variables p q r s t x l m:Prop.
-    (*Definimos las cosas que usaremos
-    para las pruebas*)
 
-    Theorem inn: p -> ~ ~ p.
-    Proof.
-        intro.
-        unfold not.
-        intro.
-        exact(H0 H).
-    Qed.
+  Variables p q r s t x l m : Prop.
 
-    Theorem deM3: ~p \/ ~ q -> ~(p /\ q).
-    Proof.
-        intros.
-        unfold not.
-        intro.
-        destruct H0.
-        destruct H.
-        -  exact (H H0).
-        -  exact (H H1).
-    Qed.
+  (* Definimos las cosas que usaremos para las pruebas *)
 
-    Lemma MP: (p -> q) -> p -> q.
-    Proof.
-        intros. 
-        apply H.
-        trivial.
-    Qed.
+  Theorem inn : p -> ~~p.
+  Proof.
+    intro H.
+    unfold not.
+    intro H0.
+    exact (H0 H).
+  Qed.
 
-    Lemma ConjI: p -> q -> p /\ q.
-    Proof.
-        intros.
-        split.
-        trivial.
-        trivial.
-    Qed.
+  Theorem deM3 : ~p \/ ~q -> ~(p /\ q).
+  Proof.
+    intros H.
+    unfold not.
+    intro H0.
+    destruct H0 as [Hp Hq].
+    destruct H as [Hnp | Hnq].
+    - exact (Hnp Hp).
+    - exact (Hnq Hq).
+  Qed.
 
-    Theorem defimp: (~p \/ q) -> p -> q.
-    Proof.
-        unfold not.
-        intros.
-        destruct H.
-        - exfalso.
-        - apply H.
-        - trivial
-        intuition.
-        - trivial.
-    Qed.
+  Lemma MP : (p -> q) -> p -> q.
+  Proof.
+    intros H Hp.
+    apply H.
+    exact Hp.
+  Qed.
 
-    Lemma DisyE: (p -> r) -> (q -> r) -> p \/ q -> r.
-    Proof.
-        intros.
-        destruct H1.
-        trivial.
-        trivial.
-    Qed.
+  Lemma ConjI : p -> q -> p /\ q.
+  Proof.
+    intros Hp Hq.
+    split.
+    - exact Hp.
+    - exact Hq.
+  Qed.
 
-    Theorem Imptrans : (p -> q) -> (q -> s) -> (p -> s).
-    Proof.
-        intro h1.
-        intro h2.
-        intro h3.
-        apply h2.
-        apply h1.
-        exact h3.
-    Qed.
+  Theorem defimp : (~p \/ q) -> p -> q.
+  Proof.
+    intros H Hp.
+    destruct H as [Hnp | Hq].
+    - exfalso.
+      apply Hnp.
+      exact Hp.
+    - exact Hq.
+  Qed.
 
-    Theorem ImpExp3: (p /\ q -> r) -> p -> q -> r.
-    Proof.
-        intros.
-        apply H.
-        split.
-        - exact H0.
-        - exact H1.
-    Qed.
+  Lemma DisyE : (p -> r) -> (q -> r) -> p \/ q -> r.
+  Proof.
+    intros Hp Hr Hpq.
+    destruct Hpq as [Hp' | Hq'].
+    - apply Hp. exact Hp'.
+    - apply Hr. exact Hq'.
+  Qed.
 
-    Theorem Disy2: q \/ p -> p \/ q.
-    Proof.
-        intros.
-        destruct H.
-        intros.
-        right.
-        assumption.
-        intros.
-        left.
-        exact H0.
-    Qed.
+  Theorem Imptrans : (p -> q) -> (q -> s) -> (p -> s).
+  Proof.
+    intros Hpq Hqs Hp.
+    apply Hqs.
+    apply Hpq.
+    exact Hp.
+  Qed.
 
-    Theorem DilemaC2 : (p -> q) -> (r -> s) -> p \/ r -> q \/ s. 
-    Proof.
-        intros.
-        destruct H1.
-        left.
-        apply H. 
-        trivial.
-        right.
-        exact (H0 H1).
-    Qed.
+  Theorem ImpExp3 : (p /\ q -> r) -> p -> q -> r.
+  Proof.
+    intros H Hp Hq.
+    apply H.
+    split.
+    - exact Hp.
+    - exact Hq.
+  Qed.
 
+  Theorem Disy2 : q \/ p -> p \/ q.
+  Proof.
+    intro H.
+    destruct H as [Hq | Hp].
+    - right. exact Hq.
+    - left. exact Hp.
+  Qed.
 
-    Theorem Distrib : p \/ (q /\ r) -> (p \/ q) /\ (p \/ r).
-    Proof.
-        intros.
-        split.
-        - destruct H.
-        + left.
-            exact H.
-        + right.
-            destruct H.
-            exact H.
-        - destruct H.
-        + left.
-            exact H.
-        + destruct H.
-            right. exact H0.
-    Qed.  
+  Theorem DilemaC2 : (p -> q) -> (r -> s) -> p \/ r -> q \/ s.
+  Proof.
+    intros Hpq Hrs Hpr.
+    destruct Hpr as [Hp | Hr].
+    - left. apply Hpq. exact Hp.
+    - right. apply Hrs. exact Hr.
+  Qed.
+
+  Theorem Distrib : p \/ (q /\ r) -> (p \/ q) /\ (p \/ r).
+  Proof.
+    intro H.
+    split.
+    - destruct H as [Hp | [Hq Hr]].
+      + left. exact Hp.
+      + right. exact Hq.
+    - destruct H as [Hp | [Hq Hr]].
+      + left. exact Hp.
+      + right. exact Hr.
+  Qed.
+
+End Ejemplos.
